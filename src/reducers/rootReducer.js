bbcec61abab23b69495197c57646e51a9422273c
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-02-18 20:03:16 +0000

    Add root reducer

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux'
+import loadingReducer from './loadingReducer'
+import articlesReducer from './articlesReducer'
+import commentsReducer from './commentsReducer'
+
+const rootReducer = combineReducers({
+  loading: loadingReducer,
+  articles: articlesReducer,
+  comments: commentsReducer
+})
