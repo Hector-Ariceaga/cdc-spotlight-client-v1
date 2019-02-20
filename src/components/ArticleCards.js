7cf9c957e75fe861dbf605838c4bd374c3c66584
Hector-Ariceaga <crablogni@hotmail.com>
2019-02-19 21:14:40 -0800

    Add container to hold article cards

---
+++ src/components/ArticleCards.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import ArticleCard from './ArticleCard'
+
+class ArticleCards extends Component {
+    
+    render () {
+        
+        function isEmpty(obj) {
+            return Object.keys(obj).length === 0;
+        }
+
+        if (isEmpty(this.props.articles)) {
+            return(<div>No Articles</div>)
+        } else {
+            const renderCards = this.props.articles.map((article, idx) => <ArticleCard key={idx} article={article}/>)
+
+            return (
+                <div className='article_cards'>
+                {renderCards}
+            </div>
+            )
+        }
+    }
+}
+
+export default ArticleCards;
--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import ArticleCard from './ArticleCard';
+import ArticleCards from './ArticleCards'
 import { connect } from 'react-redux'
-import { fetchArticles } from './actions/articleActions'
+import { fetchArticles } from '../actions/articleActions'
+
 
 class Articles extends Component {
 
@@ -10,19 +11,19 @@ class Articles extends Component {
     }
 
       render(){
-        const { articles } this.props
-        const renderCards = articles.map((article, idx) => <ArticleCard key={idx} article={article}/>)
+        const { articles } = this.props
 
         return(
             <div>
                 <h2>Articles</h2>
-                {renderCards}
+                <ArticleCards articles={articles} />
             </div>
         )
       }
 }
 
-const mapStateToProps = state = {
-  articles: state.articles
+const mapStateToProps = state => {
+ return {articles: state.articles} 
 }
+
 export default connect (mapStateToProps, {fetchArticles})(Articles)
