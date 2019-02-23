5cc3d55d5155d8348e14d2407a037f3a4485e610
Hector-Ariceaga <crablogni@hotmail.com>
2019-02-23 12:50:03 -0800

    Add response if no comments are found for article

--- src/containers/CommentsContainer.js
+++ src/containers/CommentsContainer.js
@@ -6,17 +6,23 @@ import Comments from '../components/Comments'
 
 
 class CommentsContainer extends Component {
-
+    
     handleOnClick = () => {
         this.props.fetchComments(this.props.articleId)
     }
 
+
     render(){
+        let noComments;
+        if (this.props.comments.length === 0) {
+            noComments = <div>There are currently no comments for this article.</div>
+        }
             return(
                 <div>
                     <h2>Comments</h2>
                     <button onClick={this.handleOnClick}>Load Comments</button>
                     <Comments article={this.props.articleId} />
+                    {noComments}
                 </div>
             )
      }
