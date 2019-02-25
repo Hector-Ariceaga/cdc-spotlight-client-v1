54ea21c1e6770429b70c209acd5a63991cb805e2
Hector-Ariceaga <crablogni@hotmail.com>
2019-02-24 19:21:00 -0800

    Add CSS to about page and small CSS change on home page

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,22 @@ import React from 'react';
 
 const About = () => {
     return (
-        <p>This is a detailed section that discussses Spotlight.</p>
+        <div className='about-backdrop fill'>
+            <div>
+                <div className='row'>
+                    <div className='col-md-4'>
+                    </div>
+                    <div className='col-md-4 about rounded' >
+                        <p className='lg-text'>Thank you for supporting your favorite article cataloger, Spotlight.</p>
+                        <br/>
+                        <p className='md-text'>This application was developed in 2019 by Hector Ariceaga as way of viewing and cataloging articles.</p>
+                        <p>Please stay tuned for additional features...</p>
+                    </div>
+                    <div className='col-md-4'>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,16 +4,15 @@ import { NavLink } from 'react-router-dom';
 const Home = () => {
     
     return(
-        <div className='backdrop' id='jumbotron'>
-            <div class='container ' >
-            <div className='row'> 
-                <div className='col-md6 mx-auto my-5 ' >
-                <h1 className='light-header'>Welcome to Spotlight!</h1>
-                
-                <NavLink to='/articles' exact><button type="button" className="btn btn-primary btn-lg btn-secondary">Start Reading!</button></NavLink>
+        <div className='backdrop fill'>
+            <div class='container' >
+                <div className='row'> 
+                    <div className='col-md6 mx-auto my-5' >
+                        <h1 className='light-header'>Welcome to Spotlight!</h1>
+                        
+                        <NavLink to='/articles' exact><button type="button" className="btn btn-primary btn-lg btn-secondary">Start Reading!</button></NavLink>
+                    </div>
                 </div>
-            </div>
-           
             </div>
         </div>
     )
